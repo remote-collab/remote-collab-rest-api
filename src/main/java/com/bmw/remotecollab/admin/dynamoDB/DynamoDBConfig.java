5acac906cc83387ce763e559b07cc7b7ce18ebcc
Daniel Stahr <daniel.stahr@iteratec.com>
2019-08-02 12:22:35 +0200

    Added region to aws config.

--- src/main/java/com/bmw/remotecollab/admin/dynamoDB/DynamoDBConfig.java
+++ src/main/java/com/bmw/remotecollab/admin/dynamoDB/DynamoDBConfig.java
@@ -7,6 +7,9 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.bmw.remotecollab.admin.service.OpenViduService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -17,6 +20,8 @@ import org.springframework.context.annotation.Configuration;
 @EnableDynamoDBRepositories (basePackages = "com.bmw.remotecollab.admin.dynamoDB")
 public class DynamoDBConfig {
 
+    private static final Logger logger = LoggerFactory.getLogger(DynamoDBConfig.class);
+
     @Value("${amazon.dynamodb.endpoint}")
     private String dynamoDBEndpoint;
 
@@ -26,12 +31,18 @@ public class DynamoDBConfig {
     @Value("${amazon.aws.secretkey}")
     private String dynamoDBSecret;
 
+
+    @Value("${amazon.aws.region}")
+    private String region;
+
     @Bean
     public AmazonDynamoDB amazonDynamoDB() {
+        logger.info("STarting DynamoDB with endpoint: {}", this.dynamoDBEndpoint);
         return AmazonDynamoDBClientBuilder
                 .standard()
-                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(dynamoDBEndpoint, "local"))
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(dynamoDBEndpoint, region))
                 .withCredentials(amazonAWSCredentialsProvider())
+                // .withRegion("eu-west-1")
                 .build();
     }
 
